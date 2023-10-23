ea4c5c7aea01116803baef6accbf9e14db7cd021
Your Name <tajempey@gmail.com>
2023-10-23 14:08:06 -0600

    Insert array values into an unordered list
    Co-authored-by: Monica Barboza <monboza@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,9 @@
+window.addEventListener("load", function() {
 const flavors = [1, 2, 3, 4 , 5]
-//const li = document.createElement("li");
-const ul = document.querySelector("ul#difFlavors");
+const ul = document.querySelector("ul");
 flavors.forEach(function(flavor) {
-console.log(flavor);
-const li = document.createElement("li"); 
+const li = document.createElement("li");
 li.append(flavor);
 ul.append(li);
-console.log(ul);
 });
-
-window.addEventListener("load", function () {
-});
-
-
-const flavors = [1, 2, 3, 4 , 5]
-const li = document.createElement("li");
-flavors.forEach(function(flavor) {
-console.log(flavor);
-const li = document.createElement("li"); 
-li.append(flavor);
-console.log(li);
 });
